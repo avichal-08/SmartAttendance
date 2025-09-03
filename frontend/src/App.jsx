b6a6f9a0678eb205029c3df171e5b6a941709529
Avichal Pandey <avichal2018lko@gmail.com>
2025-09-03 19:53:56 +0530

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,54 @@
+import { useState,useEffect } from 'react'
+
+function App() {
+   const centre={lat:26.9110492,lng:80.9695695};
+   const [user,setUser]=useState({lat:'',lng:''});
+   const [inside,setInside]=useState(false);
+
+    useEffect(() => {
+    navigator.geolocation.getCurrentPosition((pos) => {
+      const {latitude,longitude}=pos.coords;
+      setUser({lat:latitude,lng:longitude});
+    } ,null,{
+    enableHighAccuracy: true,  
+    timeout: 10000,
+    maximumAge: 0  
+  });
+  }, []);
+
+  function isInsideGeofence(userLat, userLng, centerLat, centerLng, radiusMeters) {
+    const toRad=(x)=>(x*Math.PI)/180;
+    const R=6371000;
+    const dLat=toRad(centerLat-userLat);
+    const dLng=toRad(centerLng-userLng);
+    const a =
+      Math.sin(dLat/2)**2 +
+      Math.cos(toRad(userLat))*
+        Math.cos(toRad(centerLat))*
+        Math.sin(dLng/2)**2;
+    const c =2*Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    return R*c<=radiusMeters;
+  }
+
+  function onClickHandler(){
+    const check=isInsideGeofence(user.lat,user.lng,centre.lat,centre.lng,50);
+    const result=check?"You are inside geofencing":"You are outside geofencing";
+    setInside(result);
+  }
+
+  return (
+    <div className='bg-black w-full h-screen p-2'>
+      <button onClick={onClickHandler} className='w-full md:w-50 h-20 text-3xl text-white cursor-pointer rounded-4xl mt-20 md:ml-[20%] bg-blue-500'>Click here</button>
+      {inside&&<div className='w-full md:w-[40%] h-20 rounded-4xl text-4xl text-white pt-3 pl-4 mt-20 md:ml-[20%] bg-blue-500'>
+        {inside}
+        </div>}
+      {inside&&<div className='w-full md:w-[40%]  rounded-4xl text-4xl text-white py-3 pl-4 mt-20 md:ml-[20%] bg-blue-500'>
+        <h1>Your Coordinates:</h1>
+        <p>Latitude:{user.lat}</p>
+        <p>Longitude:{user.lng}</p>
+        </div>}  
+    </div>
+  )
+}
+
+export default App
